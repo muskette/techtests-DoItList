67c7c8747d1f5467b0e0b3044bc9c8758065b2bf
will426 <laffan426@gmail.com>
2016-06-11 19:14:01 +0100

    replaced backref with related_name. backref is from SQLAlchemy, not Django

--- todo/models.py
+++ todo/models.py
@@ -6,14 +6,14 @@ from .managers import ItemManager
 
 class TodoList(models.Model):
     title = models.CharField(max_length=20)
-    owner = models.ForeignKey(User, backref='lists')
+    owner = models.ForeignKey(User, related_name='lists')
 
 class Item(models.Model):
     item_name = models.CharField(max_length=20)
     item_desc = models.CharField(max_length=500)
     is_done = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
-    list = models.ForeignKey(TodoList, backref="items")
+    list = models.ForeignKey(TodoList, related_name="items")
 
     objects = ItemManager()
     default_manager = models.Manager()
