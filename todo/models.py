21ebea1c318391dcb6e0e208df4f05193903cc46
will426 <laffan426@gmail.com>
2016-06-11 19:00:54 +0100

    Add basic models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=20)
+    owner = models.ForeignKey(User, backref='lists')
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=20)
+    item_desc = models.CharField(max_length=500)
+    is_done = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+    list = models.ForeignKey(TodoList, backref="items")
